440cd8dc704e992abdbb63f2e00ae2c58f415016
Matias <24mlaiseca@iesrioarba.es>
2026-01-30 12:38:20 +0100

    Funcion basica

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const titulo = document.querySelector('h1');
+
+  titulo.addEventListener('click', () => {
+    titulo.textContent = 'Has hecho clic en el título ';
+  });
+});
